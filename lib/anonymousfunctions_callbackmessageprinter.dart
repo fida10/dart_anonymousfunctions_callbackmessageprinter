0fdb04a5440cdf46269463d0101f39e2905d063f
eshad <fidaeshad10@gmail.com>
2023-11-26 05:05:14 -0600

    question invalid

--- lib/anonymousfunctions_callbackmessageprinter.dart
+++ lib/anonymousfunctions_callbackmessageprinter.dart
@@ -5,3 +5,18 @@ library;
 
 export 'src/anonymousfunctions_callbackmessageprinter_base.dart';
 
+/*
+Implement a function onButtonPress that takes a callback function 
+as a parameter. This callback function should be an anonymous function 
+that receives a String message and prints it. 
+The onButtonPress function simulates a button press event and 
+invokes the callback with a custom message.
+ */
+String Function(String x) mockPrint = (x) {
+  print(x);
+  return x;
+};
+
+onButtonPress(Function(String x) callback) {
+  callback();
+}
--- test/anonymousfunctions_callbackmessageprinter_test.dart
+++ test/anonymousfunctions_callbackmessageprinter_test.dart
@@ -2,15 +2,46 @@ import 'package:anonymousfunctions_callbackmessageprinter/anonymousfunctions_cal
 import 'package:test/test.dart';
 
 void main() {
-  group('A group of tests', () {
-    final awesome = Awesome();
+  group('Callback Message Printer', () {
+    test('Callback prints the correct message', () {
+      String printedMessage = '';
 
-    setUp(() {
-      // Additional setup goes here.
+      // Pass the anonymous function as a callback
+      onButtonPress((message) {
+        mockPrint(message);
+      });
+
+      // Test that the correct message is printed
+      expect(printedMessage, equals('Test Message'));
     });
 
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
+    test('Callback with different message', () {
+      String printedMessage = '';
+
+      void onButtonPress(void Function(String) callback) {
+        callback('Another Message');
+      }
+
+      onButtonPress((message) {
+        mockPrint(message);
+      });
+
+      expect(printedMessage, equals('Another Message'));
+    });
+
+    test('No callback invocation results in no message', () {
+      String printedMessage = '';
+
+      void onButtonPress(void Function(String)? callback) {
+        if (callback != null) {
+          callback('Message');
+        }
+      }
+
+      // Not passing a callback
+      onButtonPress(null);
+
+      expect(printedMessage, isEmpty);
     });
   });
 }
